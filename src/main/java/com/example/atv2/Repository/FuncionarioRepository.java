659db66d5e63a1a3dc6d6f051e1523d0e952fc91
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-04-19 17:24:52 -0300

    At3

---
+++ src/main/java/com/example/atv2/Repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.atv2.Repository;
+
+import com.example.atv2.Entity.Funcionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
+
+}
